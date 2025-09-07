403a733c002210aa2f1ec2049040d2696e1796ed
Sravani <nomulasravani07@gmail.com>
2025-09-07 16:09:51 +0530

    removed unnecessary code

--- proj1.py
+++ proj1.py
@@ -4,7 +4,8 @@ a = int(input("enter time in sec:"))
 for x in reversed(range(1, a + 1)):
     sec = x % 60
     min = int((x / 60)) % 60
-    hrs = int((x / 3600)) % 60
+    hrs = int((x / 3600))
     print(f"{hrs:02}:{min:02}:{sec:02}")
     time.sleep(1)
 print("...........time's up............")
+
